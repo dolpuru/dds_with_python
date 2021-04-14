9b8148f4a8a985305f6c1e51ec37dc373dd27f57
wo7864 <33424923+wo7864@users.noreply.github.com>
2021-04-14 18:36:38 +0900

    Create 9252.py
    답을 봐버렸다.
    나는 멍청이다.

---
+++ jyj/9252.py
@@ -0,0 +1,18 @@
+str1 = input()
+str2 = input()
+
+dp = [["" for i in range(len(str2)+1)] for j in range(len(str1)+1)]
+
+for i in range(1, len(str1)+1):
+    for j in range(1, len(str2)+1):
+        if str1[i-1] == str2[j-1]:
+            dp[i][j] = dp[i-1][j-1] + str1[i-1]
+        else:
+            if len(dp[i-1][j]) >= len(dp[i][j-1]):
+                dp[i][j] = dp[i-1][j]
+            else:
+                dp[i][j] = dp[i][j-1]
+
+print(len(dp[-1][-1]))
+if len(dp[-1][-1]) > 0:
+    print(dp[-1][-1])
