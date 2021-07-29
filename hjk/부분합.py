fa469d2a41830bd46c0f07c71304475c02ceb757
DESKTOP-I6BO71Q\Hellllo <must1080@naver.com>
2021-07-29 15:04:23 +0900

    부분합, 공부하기

--- hjk/부분합.py
+++ hjk/부분합.py
@@ -1,3 +1,39 @@
+'''
+실패
+'''
+# import sys
+# input = sys.stdin.readline
+# n, m = map(int, input().split())
+# list_ = list(map(int, input().split()))
+
+
+# def two_pointer (lp, rp, n, m):
+#     answer = sys.maxsize
+#     temp = 0
+#     while rp < n:
+
+#         if temp >= m:
+#             answer = min(answer, rp - lp)
+#             temp -= list_[lp]
+#             lp += 1
+            
+#         elif temp < m:
+#             temp += list_[rp]
+#             rp += 1
+            
+#     return answer
+
+
+# answer = two_pointer(0,0, n, m)
+# if answer == sys.maxsize:
+#     print(0)
+# else:
+#     print(answer)
+# 차이는 while의 조건인데 ..
+# 반례 사이트 https://bingorithm.tistory.com/13
+'''
+통과
+'''
 import sys
 input = sys.stdin.readline
 n, m = map(int, input().split())
@@ -5,14 +41,25 @@ list_ = list(map(int, input().split()))
 
 
 def two_pointer (lp, rp, n, m):
-    answer = 100001
-    while rp < n:
-        temp = sum(list_[lp : rp+1]) # 이부분에서 시간초과
+    answer = sys.maxsize
+    temp = 0
+    while True:
 
-        if temp > m:
+        if temp >= m:
+            answer = min(answer, rp - lp)
+            temp -= list_[lp]
             lp += 1
-        elif temp <= m:
-            answer = min(answer, rp + 1 - lp)
+        elif rp == n:
+            break
+        elif temp < m:
+            temp += list_[rp]
             rp += 1
+            
     return answer
-print(two_pointer(0,0, n, m))
+
+
+answer = two_pointer(0,0, n, m)
+if answer == sys.maxsize:
+    print(0)
+else:
+    print(answer)
