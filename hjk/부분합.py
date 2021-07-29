99ec19bcea8d92053d74b89157ad32c1fbe9f8cf
DESKTOP-I6BO71Q\Hellllo <must1080@naver.com>
2021-07-29 14:49:25 +0900

    부분합 , 시간초과

--- hjk/부분합.py
+++ hjk/부분합.py
@@ -1,1 +1,18 @@
-print("test")
+import sys
+input = sys.stdin.readline
+n, m = map(int, input().split())
+list_ = list(map(int, input().split()))
+
+
+def two_pointer (lp, rp, n, m):
+    answer = 100001
+    while rp < n:
+        temp = sum(list_[lp : rp+1]) # 이부분에서 시간초과
+
+        if temp > m:
+            lp += 1
+        elif temp <= m:
+            answer = min(answer, rp + 1 - lp)
+            rp += 1
+    return answer
+print(two_pointer(0,0, n, m))
