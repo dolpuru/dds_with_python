7456072d8204153176f452ce03092efe2ee77c7a
DESKTOP-I6BO71Q\Hellllo <must1080@naver.com>
2021-07-30 17:30:00 +0900

    짝지어 제거하기 clear

--- hjk/프로그래머스/lv2/짝지어 제거하기.py
+++ hjk/프로그래머스/lv2/짝지어 제거하기.py
@@ -1,58 +1,11 @@
-# # 시간초과
-# # def solution(s): # 지우고 또 지워야해서 개수 세어서 푸는건 불가.
-# #     answer = -1
-# #     s = s.upper()
-# #     check_list = []
+def solution(s):
     
-# #     i = 0
-# #     while i < len(s)-1:
-# #         if s[i] == s[i+1]:
-# #             s = s[:i] + s[i+2:]
-# #             i = 0
-# #             continue
-# #         i += 1
-        
-# #     if s:
-# #         answer = 0
-# #     else:
-# #         answer = 1
-
-# #     return answer
-
-# def solution(s): # 지우고 또 지워야해서 개수 세어서 푸는건 불가.
-#     answer = -1
-#     s = s.upper()
-#     check_list = []
+    list_ = ["A"]           # 쉽게 비교하기 위해 될 수 없는 값을 넣어 놨다.
     
-#     i = 0
-#     while i < len(s)-1:
-#         if s[i] == s[i+1]:
-#             s = s[:i] + s[i+2:]
-#             i = 0
-#             continue
-#         i += 1
-        
-#     if s:
-#         answer = 0
-#     else:
-#         answer = 1
-
-#     return answer
-
-import re
-a = "aabbc1"
-c = "cdcd"
-def solution(s): # 지우고 또 지워야해서 개수 세어서 푸는건 불가.
-    answer = re.sub("([a-z]){2})","",s)
-    while True:
-        temp = re.sub("([a-z]){2}","",answer)
-        answer = re.sub("([a-z]){2}","",temp)
-        if answer == temp:
-            break
-    print(answer)       
-    if len(answer) == 0:
-        return 1
-    else:
-        return 0
-print(solution(a))
-print(solution(c))
+    for i in s:             # s를 돌면서
+        if i == list_[-1]:  # 젤 끝이랑 같으면 pop
+            list_.pop()
+        else:               # 아니면 append
+            list_.append(i)
+    
+    return (1 if len(list_) == 1 else 0) 
