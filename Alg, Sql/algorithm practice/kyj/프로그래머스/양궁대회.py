5749ab40edb9bdedd520ed80899e6ff03c8f8705
yujeongkwon <yoojeongkwon@naver.com>
2022-06-30 02:27:57 +0900

    2

--- Alg, Sql/algorithm practice/kyj/프로그래머스/양궁대회.py
+++ Alg, Sql/algorithm practice/kyj/프로그래머스/양궁대회.py
@@ -1,27 +1,38 @@
-
+import copy            
 def solution(n, info):
     answer = [0,0,0,0,0,0,0,0,0,0,0]
     count, a_score, l_score= 0,0,0
-
+    max_  = 0 
+    
     for i in range(10):
         if info[i]: a_score += 10-i
     
+    def dfs(a_score,l_score, idx,li):
+        temp = copy.deepcopy(li)
+        if idx >= 11:
+            nonlocal max_
+            nonlocal answer
+            if max_ < l_score - a_score:
+                max_ = l_score - a_score
+                if sum(temp) < n:   temp[-1] = n - sum(temp)
+                answer = temp
+            return
+        else:
+            #피하고 다른 것
+            dfs(a_score,l_score,idx+1,temp)
+            # 어피치 것 뻇기
+            if sum(temp) + info[idx] +1 <= n and info[idx] != 0:
+                a_score -= 10- idx
+                l_score += 10 - idx
+                temp[idx] = info[idx] + 1
+                dfs(a_score,l_score,idx+1,temp)
+            elif info[idx] == 0:    #어피치가 안 쏜것
+                l_score += 10 - idx
+                temp[idx] = 1
+                dfs(a_score,l_score,idx+1,temp)
     
+    dfs(a_score,l_score,0,answer)
     
-    return answer
+    return [-1] if answer == [0,0,0,0,0,0,0,0,0,0,0] else answer
 
-answer = [0,0,0,0,0,0,0,0,0,0,0]
-
-# 1. 어피치보다 1개화살 더 많이 쏴서 
-# 	어피치점수 - (10-i)
-# 	내점수 + 10+i
-
-# 2. 어피치가 쏜 것 피하여 화살을 아낀 후 다른 곳에 쏘기
-
-# 1, 2 번의 경우를 둘다 기술 -> 그래프문제 DFS,BFS 더 나은 것
-
-# 3. 화살이 n 보다 많다면 멈추기
-
-# answer을 추가하면서 info 계속 비교
-
-# 4. 차이 값 max_ 기록하기
+print(solution(10,[0,0,0,0,0,0,0,0,3,4,3]))
