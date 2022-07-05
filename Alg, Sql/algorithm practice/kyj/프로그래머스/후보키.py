3d0675a72ae1d542839671dcb67d8e887c9c4374
yujeongkwon <yoojeongkwon@naver.com>
2022-07-06 00:17:25 +0900

    gg

--- Alg, Sql/algorithm practice/kyj/프로그래머스/후보키.py
+++ Alg, Sql/algorithm practice/kyj/프로그래머스/후보키.py
@@ -1,3 +1,44 @@
+from itertools import combinations
+import re
+
+
 def solution(relation):
-    answer = 0
-    return answer
+    answer = []
+    cal = [i for i in range(len(relation[0]))]
+    def dis(c,relation):
+        nonlocal answer
+        cals = []
+        set_ = []
+        for i in c: cals.append([j[i] for j in relation])
+
+        for i in range(len(relation)):
+            temp = []
+            for j in range(len(cals)):
+                temp.append(cals[j][i])
+            set_.append(tuple(temp))
+        
+        if len(set_) == len(set(set_)): return 1
+        else:    return 0
+
+    for i in range(1,len(cal)+1):
+        for c in combinations(cal,i):
+            if len(answer) == 0:
+                if dis(c,relation): answer.append(c)
+            else:
+                for li in answer:    
+                    temp = set()
+                    temp.update(li)
+                    temp.update(c)
+                    if len(temp) != len(li)+ len(c): break
+                else:    
+                    if dis(c,relation): answer.append(c)
+
+    return len(answer)
+
+print(solution([["100","ryan","music","2"],["200","apeach","math","2"],["300","tube","computer","3"],["400","con","computer","4"],["500","muzi","music","3"],["600","apeach","music","2"]]))
+
+# len(temp) = 2
+# [['100', '200', '300', '400', '500', '600'],
+#  ['music', 'math', 'computer', 'computer', 'music', 
+# 'music']]
+# temp[k][0]
