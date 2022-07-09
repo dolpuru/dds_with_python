d1670f6256e55f6bde8c1b20843cb087eaf25486
yujeongkwon <yoojeongkwon@naver.com>
2022-07-09 23:23:38 +0900

    gg

---
+++ Alg, Sql/algorithm practice/kyj/프로그래머스/괄호회전하기.py
@@ -0,0 +1,12 @@
+def solution(s):
+    stack = []
+    answer = -1
+    def bracket(s):
+        
+    for i in s:
+        if len(stack) ==0:  stack.append(i)
+        else:
+            if 
+    return answer
+
+print(solution("[](){}"))
