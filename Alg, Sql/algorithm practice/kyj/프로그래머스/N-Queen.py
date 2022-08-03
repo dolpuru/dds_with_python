f016b9f42a8a9763706c9107887e65969c073f62
yujeongkwon <yoojeongkwon@naver.com>
2022-08-03 23:48:49 +0900

    n퀸 다시풀기

--- Alg, Sql/algorithm practice/kyj/프로그래머스/N-Queen.py
+++ Alg, Sql/algorithm practice/kyj/프로그래머스/N-Queen.py
@@ -1,24 +1,44 @@
 def solution(n):
     answer = 0
-    row = [0] * n
-
-    def Queen(y):
+    board = [0 for i in range(n)]
+    
+    def Queen(count):
         nonlocal answer
-        if y == n:
-            answer +=1
-            return
+        if count == n:
+            answer += 1
+            return 
         else:
-            for i in range(n):
-                row[y] = i
-                # 넣어서 되는지 확인
-                for j in range(y):
-                    if row[y] == row[j] or abs((row[y] - row[j]) / (y - j)) == 1:
+            for x in range(n):
+                board[count] = x
+                for y in range(count):
+                    if board[y] == x or abs((x-board[y])/(count-y)) == 1:
                         break
-                else:
-                    Queen(y+1)
-
+                else: Queen(count+1)
     Queen(0)
-
     return answer
 
-print(solution(4))
+
+# def solution(n):
+#     answer = 0
+#     row = [0] * n
+
+#     def Queen(y):
+#         nonlocal answer
+#         if y == n:
+#             answer +=1
+#             return
+#         else:
+#             for i in range(n):
+#                 row[y] = i
+#                 # 넣어서 되는지 확인
+#                 for j in range(y):
+#                     if row[y] == row[j] or abs((row[y] - row[j]) / (y - j)) == 1:
+#                         break
+#                 else:
+#                     Queen(y+1)
+
+#     Queen(0)
+
+#     return answer
+
+# print(solution(4))
