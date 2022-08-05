51c98172c877f6087179193ede7c4d231416d7c5
yujeongkwon <yoojeongkwon@naver.com>
2022-08-05 23:46:33 +0900

    하노이의탑 다시

--- Alg, Sql/algorithm practice/kyj/프로그래머스/하노이의탑.py
+++ Alg, Sql/algorithm practice/kyj/프로그래머스/하노이의탑.py
@@ -1,14 +1,28 @@
-answer = []
-def hanoi(n,st,sp,end):
-    global answer
-    if n == 1:
-        answer.append([st,end])
-        return
-    else:
-        hanoi(n-1,st,end,sp)
-        answer.append([st,end])
-        hanoi(n-1,sp,st,end)
-
+#현재
 def solution(n):
+    answer = []
+    def hanoi(n,st,sp,end):
+        nonlocal answer
+        if n == 1:  answer.append([st,end])
+        else:
+            hanoi(n-1,st,end,sp)
+            answer.append([st,end])
+            hanoi(n-1,sp,st,end)
     hanoi(n,1,2,3)
     return answer
+
+# 옛날 코드
+#  answer = []
+# def hanoi(n,st,sp,end):
+#     global answer
+#     if n == 1:
+#         answer.append([st,end])
+#         return
+#     else:
+#         hanoi(n-1,st,end,sp)
+#         answer.append([st,end])
+#         hanoi(n-1,sp,st,end)
+
+# def solution(n):
+#     hanoi(n,1,2,3)
+#     return answer
