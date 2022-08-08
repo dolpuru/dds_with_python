1062a99fa9a92f3cf91babe984bd77043c41d301
yujeongkwon <yoojeongkwon@naver.com>
2022-08-08 18:13:48 +0900

    gg

--- Alg, Sql/algorithm practice/kyj/프로그래머스/최댓값과 최솟값.py
+++ Alg, Sql/algorithm practice/kyj/프로그래머스/최댓값과 최솟값.py
@@ -1,11 +1,13 @@
-s= "1 2 3 4"
-answer = ''
+ # def solution(s):
+#     answer = ''
 
-s = s.split()
-for i in range(len(s)):
-    s[i] = int(s[i])
+#     s = s.split()
+#     for i in range(len(s)):
+#         s[i] = int(s[i])
 
-
-s.sort()
-answer = str(s[0]) + " " + str(s[-1])
-print( answer)
+#     s.sort()
+#     answer = str(s[0]) + " " + str(s[-1]) 
+#     return answer
+def solution(s):
+    s = list(map(int,s.split()))
+    return str(min(s)) + " "+ str(max(s))
